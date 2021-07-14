030b035ff6a85d2da726fe2e04f7cb0482bc59b4
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-07-14 14:57:53 -0500

    Added main fragments
    Still missing content, and extra fragments like map/feed view and friends fragmetn page

--- app/src/main/java/com/example/safeout/activities/MainActivity.java
+++ app/src/main/java/com/example/safeout/activities/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.safeout.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.safeout.R;
+import com.example.safeout.fragments.MapFragment;
+import com.example.safeout.fragments.StatusFragment;
+import com.example.safeout.fragments.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // hide action bar (this only contains the name of the app)
         getSupportActionBar().hide();
+
+        // Inflate navbar into main activity
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navBar);
+        // Start home fragment when app opens up
+        getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, new MapFragment()).commit();
+        // switch case for switching between screens (fragments)
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.status:
+                        selectedFragment = new StatusFragment();
+                        break;
+                    case R.id.map:
+                        selectedFragment = new MapFragment();
+                        break;
+                    case R.id.user:
+                        selectedFragment = new UserFragment();
+                        break;
+                }
+                // Open selected fragment
+                getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, selectedFragment).commit();
+                return true;
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/safeout/fragments/UserFragment.java
@@ -0,0 +1,47 @@
+package com.example.safeout.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.safeout.R;
+import com.example.safeout.activities.LoginActivity;
+import com.example.safeout.activities.MainActivity;
+import com.parse.ParseUser;
+
+public class UserFragment extends Fragment {
+
+    private Button btnLogout;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnLogout = view.findViewById(R.id.btnLogOut);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOut();
+            }
+        });
+    }
+
+    private void logOut() {
+        ParseUser.logOut();
+        Intent i = new Intent(getContext(), LoginActivity.class);
+        startActivity(i);
+        ((MainActivity)getActivity()).finish();
+    }
+}
