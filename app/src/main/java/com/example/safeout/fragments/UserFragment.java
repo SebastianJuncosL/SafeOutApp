a4782255b6cb99075a011a119e0866b660604f18
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-07-15 10:36:36 -0500

    User's status will change to undefined when logging out

--- app/src/main/java/com/example/safeout/fragments/UserFragment.java
+++ app/src/main/java/com/example/safeout/fragments/UserFragment.java
@@ -2,10 +2,12 @@ package com.example.safeout.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,9 @@ import androidx.fragment.app.Fragment;
 import com.example.safeout.R;
 import com.example.safeout.activities.LoginActivity;
 import com.example.safeout.activities.MainActivity;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 public class UserFragment extends Fragment {
@@ -33,15 +38,42 @@ public class UserFragment extends Fragment {
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                logOut();
+                resetStatus();
             }
         });
     }
 
     private void logOut() {
+
         ParseUser.logOut();
         Intent i = new Intent(getContext(), LoginActivity.class);
         startActivity(i);
         ((MainActivity)getActivity()).finish();
+    }
+
+    private void resetStatus() {
+        // Database Class goes in getQuery
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("_User");
+        // objectId -> can be retrieved from ParseUser.getCurrentUser().getObjectId()
+        query.getInBackground(ParseUser.getCurrentUser().getObjectId(), (object, e) -> {
+            if (e == null) {
+                //Object was successfully retrieved
+                object.put("currentStatus", "(undefined)");
+                //All other fields will remain the same
+                try {
+                    object.save();
+                } catch (ParseException parseException) {
+                    parseException.printStackTrace();
+                }
+                Log.d("StatusFragment", object.get("currentStatus").toString());
+            } else {
+                // something went wrong
+                Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            if (object.get("currentStatus") == "(undefined)") {
+                logOut();
+            }
+        });
     }
 }
