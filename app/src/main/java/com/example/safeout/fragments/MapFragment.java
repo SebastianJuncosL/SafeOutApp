628177304264c0b3bd5207ae9f27c03a6f6c1477
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-07-14 20:38:17 -0500

    Added Map testing activity
    Having some issues with the map, but it will be later implemented in the MapFragment

--- app/src/main/java/com/example/safeout/fragments/MapFragment.java
+++ app/src/main/java/com/example/safeout/fragments/MapFragment.java
@@ -1,17 +1,22 @@
 package com.example.safeout.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.safeout.R;
+import com.example.safeout.activities.MapTestingActivity;
 
 public class MapFragment extends Fragment {
+
+    private Button btnGoToMap;
 
     @Nullable
     @Override
@@ -22,5 +27,18 @@ public class MapFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        btnGoToMap = view.findViewById(R.id.btnGoToMap);
+        btnGoToMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMap();
+            }
+        });
+
+    }
+
+    private void goToMap() {
+        Intent i = new Intent(getContext(), MapTestingActivity.class);
+        startActivity(i);
     }
 }
