0c7819b3a6fee05bf5724bff5a4d1822c1994afd
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-07-23 09:50:47 -0500

    Added recycler view for friends info (not working)

--- app/src/main/java/com/example/safeout/activities/FriendsActivity.java
+++ app/src/main/java/com/example/safeout/activities/FriendsActivity.java
@@ -1,15 +1,24 @@
 package com.example.safeout.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.safeout.R;
+import com.example.safeout.adapters.FriendInformationAdapter;
 import com.example.safeout.adapters.FriendRequestAdapter;
 import com.example.safeout.models.FriendInformation;
 import com.example.safeout.models.FriendRequest;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +34,8 @@ public class FriendsActivity extends AppCompatActivity {
     public List<FriendInformation> information;
     public List<FriendRequest> requests;
 
+    private FriendInformationAdapter friendsAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +43,47 @@ public class FriendsActivity extends AppCompatActivity {
 
         // hide action bar (this only contains the name of the app)
         getSupportActionBar().hide();
+        rvFriends = findViewById(R.id.rvFriends);
+        rvRequests = findViewById(R.id.rvRequests);
 
         information = new ArrayList<>();
         requests = new ArrayList<>();
+
+        friendsAdapter = new FriendInformationAdapter(this, information);
+
+        // set the adapter on the recycler view
+        rvFriends.setAdapter(friendsAdapter);
+        // set the layout manager on the recycler view
+        rvFriends.setLayoutManager(new LinearLayoutManager(this));
+        // query friends from User
+        queryFriends();
+    }
+
+    private void queryFriends() {
+        // specify what type of data we want to query - Post.class
+        ParseQuery<FriendInformation> query = ParseQuery.getQuery(FriendInformation.class);
+        // include data referred by user key
+        query.include(FriendInformation.userId);
+        // start an asynchronous call for posts
+        query.findInBackground(new FindCallback<FriendInformation>() {
+
+            @Override
+            public void done(List<FriendInformation> friends, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting friends", e);
+                    return;
+                }
+
+                // for debugging purposes let's print every post description to logcat
+                for (FriendInformation friend : friends) {
+                    Log.i(TAG, "Friend: " + friend.getUserName() + ", phone: " + friend.getPhoneNumber());
+                }
+
+                // save received posts to list and notify adapter of new data
+                information.addAll(friends);
+                friendsAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/safeout/models/FriendRequest.java
+++ app/src/main/java/com/example/safeout/models/FriendRequest.java
@@ -1,5 +1,9 @@
 package com.example.safeout.models;
 
-public class FriendRequest {
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("FriendRequest")
+public class FriendRequest extends ParseObject {
     // This class will get all users from the friendRequests list in the database
 }
