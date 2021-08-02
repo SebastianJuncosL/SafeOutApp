7bf749a6c9bc19edf521e508ac4c8984eba98c7c
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-08-01 22:07:43 -0500

    Got rid of some extra lines

--- app/src/main/java/com/example/safeout/adapters/FriendRequestAdapter.java
+++
@@ -1,4 +0,0 @@
-package com.example.safeout.adapters;
-
-public class FriendRequestAdapter {
-}
--- app/src/main/java/com/example/safeout/models/FriendRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.safeout.models;
-
-import com.parse.ParseClassName;
-import com.parse.ParseObject;
-
-@ParseClassName("FriendRequest")
-public class FriendRequest extends ParseObject {
-    // This class will get all users from the friendRequests list in the database
-}
